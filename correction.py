fdd17475aba12581d44059eb424d9b2a7f4f6d60
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-05-02 12:47:49 +0530

    Update correction.py

--- correction.py
+++ correction.py
@@ -1,15 +1,18 @@
-import PyPDF2
+import pytesseract
+from PIL import Image
 
-# Open the PDF file
-with open('example.pdf', 'rb') as pdf_file:
-    # Create a PdfFileReader object
-    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+# Load image
+img = Image.open('image.jpg')
 
-    # Get the number of pages in the PDF file
-    num_pages = pdf_reader.getNumPages()
+# Define the region containing the date
+x = 100
+y = 100
+width = 200
+height = 50
+date_region = (x, y, x+width, y+height)
 
-    # Loop over each page and extract the text
-    for page_num in range(num_pages):
-        page = pdf_reader.getPage(page_num)
-        page_text = page.extractText()
-        print(page_text)
+# Extract the date text from the region
+date_text = pytesseract.image_to_string(img.crop(date_region), lang='eng', config='--psm 6')
+
+# Print the extracted date text
+print(date_text)
