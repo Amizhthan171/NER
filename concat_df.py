0417550121bd91e4cf32f68755138c243ed8aecf
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-05-22 14:41:14 +0530

    Update concat_df.py

--- concat_df.py
+++ concat_df.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 data = {
     'customerKey': [1, 2, 1, 4, 4],
-    'MessageName': ['Message 1', 'Message 2', 'Message 1', 'Message 4', 'Message 5'],
-    'Action': ['open', 'open', 'delivered', 'open', 'delivered']
+    'MessageName': ['Message 1', 'Message 2', 'Message 3', 'Message 4', 'Message 5'],
+    'Action': ['delivered', 'open', 'open', 'open', 'delivered']
 }
 df = pd.DataFrame(data)
 df_sorted = df.sort_values(by=['customerKey', 'Action'])
 grouped = df_sorted.groupby('customerKey')
 df_concatenated = grouped['Action'].apply(lambda x: ', '.join(x)).reset_index()
 df_merged = pd.merge(df, df_concatenated, on='customerKey')
-df_unique = df_merged.drop_duplicates(subset=['customerKey'])
+df_unique = df_merged[df_merged['Action_x'] == 'open'].drop_duplicates(subset=['customerKey'])
 print(df_unique)
