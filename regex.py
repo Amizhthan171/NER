2193f5beb16c336bfb2cb79b6811f59bbc7aedc0
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-06-07 08:39:59 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -20,10 +20,26 @@ if __name__ == '__main__':
     # Combine all CSV files into a single DataFrame
     combined_df = pd.concat([pd.read_csv(os.path.join(directory, csv_file)) for csv_file in csv_files])
 
+    # Drop the index column if present
+    combined_df.reset_index(drop=True, inplace=True)
+
+    # Convert the file path column to a list
+    file_paths = combined_df['file_path'].tolist()
+
+    # Determine the chunk size
+    chunk_size = len(file_paths) // num_processes
+
+    # Split the file paths into chunks
+    chunks = [file_paths[i:i+chunk_size] for i in range(0, len(file_paths), chunk_size)]
+
     # Create a process pool
     with Pool(processes=num_processes) as pool:
-        # Process each row in parallel
-        results = pool.map(check_file_exists, combined_df['file_path'])
+        results = []
+
+        # Process each chunk in parallel
+        for chunk in chunks:
+            chunk_results = pool.map(check_file_exists, chunk)
+            results.extend(chunk_results)
 
     # Add the results as a new column in the combined DataFrame
     combined_df['Exists'] = results
