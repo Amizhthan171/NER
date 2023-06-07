a4890a112f82f7d932f2199a7367f2cf6c144f01
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-06-07 07:49:52 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -6,30 +6,30 @@ from multiprocessing import Pool
 def check_file_exists(file_path):
     return os.path.exists(file_path)
 
-# Process a single row
-def process_row(row):
-    file_path = row['FilePath']
-    exists = check_file_exists(file_path)
-    return exists
-
 # Main function
 if __name__ == '__main__':
-    # Read the CSV file
-    df = pd.read_csv('your_file.csv')
+    # Directory containing the CSV files
+    directory = 'path/to/your/csv/directory'
+
+    # Get the list of CSV files in the directory
+    csv_files = [file for file in os.listdir(directory) if file.endswith('.csv')]
 
     # Number of processes to utilize
     num_processes = 4
 
-    # Split the DataFrame into chunks
-    chunks = np.array_split(df, num_processes)
+    for csv_file in csv_files:
+        # Construct the full path to each CSV file
+        file_path = os.path.join(directory, csv_file)
 
-    # Create a process pool
-    pool = Pool(processes=num_processes)
+        # Read the CSV file
+        df = pd.read_csv(file_path)
 
-    # Process each chunk in parallel
-    results = pool.map(process_row, chunks)
+        # Create a process pool
+        with Pool(processes=num_processes) as pool:
+            # Process each row in parallel
+            results = pool.map(check_file_exists, df['FilePath'])
 
-    # Concatenate the results
-    df['Exists'] = pd.concat(results)
+        # Add the results as a new column in the DataFrame
+        df['Exists'] = results
 
-    # Continue processing the dataframe with the existence status
+        # Continue processing the dataframe with the existence status
