8ce1c1b24c141035f2ea821d18bd6c645f883ab1
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-06-07 07:38:25 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,15 +1,35 @@
+import pandas as pd
 import os
 from multiprocessing import Pool
 
-def count_files(directory):
-    file_count = 0
-    for root, dirs, files in os.walk(directory):
-        file_count += len(files)
-    return file_count
+# Function to check file existence
+def check_file_exists(file_path):
+    return os.path.exists(file_path)
 
+# Process a single row
+def process_row(row):
+    file_path = row['FilePath']
+    exists = check_file_exists(file_path)
+    return exists
+
+# Main function
 if __name__ == '__main__':
-    directory_path = '/path/to/directory'  # Replace with the actual directory path
-    pool = Pool(processes=os.cpu_count())
-    result = pool.apply_async(count_files, (directory_path,))
-    file_count = result.get()
-    print("Number of files:", file_count)
+    # Read the CSV file
+    df = pd.read_csv('your_file.csv')
+
+    # Number of processes to utilize
+    num_processes = 4
+
+    # Split the DataFrame into chunks
+    chunks = np.array_split(df, num_processes)
+
+    # Create a process pool
+    pool = Pool(processes=num_processes)
+
+    # Process each chunk in parallel
+    results = pool.map(process_row, chunks)
+
+    # Concatenate the results
+    df['Exists'] = pd.concat(results)
+
+    # Continue processing the dataframe with the existence status
