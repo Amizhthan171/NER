8a5c654bda161892cc276921ce7e4b65535fa1f8
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-06-08 14:52:33 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -10,24 +10,42 @@ image = Image.open(image_path)
 image_gray = image.convert("L")
 
 # Perform OCR using pytesseract on the grayscale image
-ocr_data = pytesseract.image_to_data(image_gray, output_type=pytesseract.Output.DICT)
+ocr_text = pytesseract.image_to_string(image_gray)
 
 # Assume you have already extracted the desired text using a regex
 extracted_text = "Amount: $1234"
 
 # Find the index of the desired text within the OCR results
-text_index = None
-for i, word in enumerate(ocr_data["text"]):
-    if word == extracted_text:
-        text_index = i
-        break
+match = re.search(re.escape(extracted_text), ocr_text)
+if match:
+    start_index = match.start()
+    end_index = match.end()
 
-# Retrieve the bounding box coordinates if the text is found
-if text_index is not None:
-    left = ocr_data["left"][text_index]
-    top = ocr_data["top"][text_index]
-    width = ocr_data["width"][text_index]
-    height = ocr_data["height"][text_index]
+    # Iterate over the OCR text to find the bounding box
+    left = top = width = height = 0
+    char_count = 0
+    for i, char in enumerate(ocr_text):
+        if char_count == start_index:
+            left = i
+        if char_count == end_index - 1:
+            width = i - left + 1
+            height = 1
+            break
+        if char != '\n':
+            char_count += 1
+
+    # Calculate the top coordinate based on line breaks
+    lines = ocr_text.split('\n')
+    line_count = 0
+    for line in lines:
+        if char_count <= len(line):
+            top = line_count
+            break
+        else:
+            char_count -= len(line) + 1
+            line_count += 1
+
+    # Construct the bounding box coordinates
     bounding_box = (left, top, left + width, top + height)
     print("Bounding box:", bounding_box)
 else:
