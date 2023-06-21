c90533c4db2bd999adef33273cc8e03e0e93ff55
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-06-21 15:26:36 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import zipfile
+import os
 
 # Read the CSV file into a pandas DataFrame
 df = pd.read_csv('your_file.csv')
@@ -7,25 +8,28 @@ df = pd.read_csv('your_file.csv')
 # Group the rows by parent ID
 grouped = df.groupby('parent ID')
 
+# Specify the directory to save the zip files
+save_directory = '/path/to/save/directory/'
+
 # Create a new DataFrame to store zipping status
 zipping_status_df = pd.DataFrame(columns=['parent ID', 'Zipping Status'])
 
 # Iterate over each group
 for parent_id, group in grouped:
-    # Create a zip file with the parent ID as the name
-    zip_file_name = f'{parent_id}.zip'
+    # Create a zip file with the parent ID as the name in the specified directory
+    zip_file_path = os.path.join(save_directory, f'{parent_id}.zip')
     try:
-        with zipfile.ZipFile(zip_file_name, 'w') as zipf:
+        with zipfile.ZipFile(zip_file_path, 'w') as zipf:
             # Iterate over the rows in the group
             for _, row in group.iterrows():
                 file_path = row['PATHS']
                 # Add the file to the zip file
                 zipf.write(file_path)
-        # If zipping is successful, record the status as 'pass'
-        zipping_status_df = zipping_status_df.append({'parent ID': parent_id, 'Zipping Status': 'pass'}, ignore_index=True)
+        # If zipping is successful, record the status as 'success'
+        zipping_status_df = zipping_status_df.append({'parent ID': parent_id, 'Zipping Status': 'success'}, ignore_index=True)
     except:
-        # If zipping fails, record the status as 'fail'
-        zipping_status_df = zipping_status_df.append({'parent ID': parent_id, 'Zipping Status': 'fail'}, ignore_index=True)
+        # If zipping fails, record the status as 'failure'
+        zipping_status_df = zipping_status_df.append({'parent ID': parent_id, 'Zipping Status': 'failure'}, ignore_index=True)
 
 # Write the zipping status DataFrame to a CSV file
 zipping_status_df.to_csv('zipping_status.csv', index=False)
