94d937a6dffd02b9533e13687a4db45a0970527a
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-07-03 15:04:25 +0530

    Update correction.py

--- correction.py
+++ correction.py
@@ -1,18 +1,15 @@
-import pytesseract
-from PIL import Image
+import pandas as pd
 
-# Load image
-img = Image.open('image.jpg')
+data = {
+    'address': {'abc', 1, 857, 800, 600, 500, 95},
+    'name': {'def', 2, 123, 456, 789, 100, 80}
+}
 
-# Define the region containing the date (top left corner)
-x = 0
-y = 0
-width = 200
-height = 50
-date_region = (x, y, x+width, y+height)
+df = pd.DataFrame(data).transpose().reset_index()
+df.columns = ['key', 'value']
 
-# Extract the date text from the region
-date_text = pytesseract.image_to_string(img.crop(date_region), lang='eng', config='--psm 6')
+df[['address', 'page_no', 'bbox1', 'bbox2', 'bbox3', 'bbox4', 'probability']] = pd.DataFrame(df['value'].tolist())
 
-# Print the extracted date text
-print(date_text)
+df = df.drop(columns=['value'])
+
+print(df)
