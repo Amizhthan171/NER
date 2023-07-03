91040634b082511e786be5a2ebc34f5851e37a1e
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-07-03 16:23:27 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,20 +1,34 @@
-from PIL import Image
+import PyPDF2
 
-# Assuming you have the image path
-image_path = "path/to/your/image.jpg"
+# Assuming you have the PDF file path
+pdf_path = "path/to/your/file.pdf"
 
-# Load the image
-image = Image.open(image_path)
+# Create a PDF reader object
+pdf_reader = PyPDF2.PdfFileReader(pdf_path)
 
 # Iterate over the dataframe rows
 for index, row in df.iterrows():
+    # Extract the page number
+    page_no = row['PAGE NO']
+
+    # Get the page object from the PDF reader
+    page = pdf_reader.getPage(page_no)
+
     # Extract the bounding box values
     bbox = row['BBOX']
     x_min, y_min, x_max, y_max = bbox
 
-    # Crop the image based on the bounding box values
-    cropped_image = image.crop((x_min, y_min, x_max, y_max))
+    # Crop the page based on the bounding box values
+    cropped_page = page.crop((x_min, y_min, x_max, y_max))
 
-    # Save the cropped image using the key name
+    # Create a new PDF writer object
+    pdf_writer = PyPDF2.PdfFileWriter()
+
+    # Add the cropped page to the PDF writer
+    pdf_writer.addPage(cropped_page)
+
+    # Save the cropped page as a new PDF file using the key name
     key_name = row['KEY']
-    cropped_image.save(f"cropped_{key_name}.jpg")
+    output_path = f"cropped_{key_name}.pdf"
+    with open(output_path, "wb") as output_file:
+        pdf_writer.write(output_file)
