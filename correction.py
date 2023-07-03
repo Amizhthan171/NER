c673fcd21d373304c8acf1fd90f2864147440ad9
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-07-03 15:09:55 +0530

    Update correction.py

--- correction.py
+++ correction.py
@@ -8,10 +8,11 @@ data = {
 df = pd.DataFrame(columns=['KEY', 'VALUE', 'BBOX', 'PAGE NO', 'PROBABILITY'])
 
 for key, value in data.items():
-    address = value.pop(0)
-    page_no = value.pop(0)
-    bbox = value[:4]
-    probability = value[-1]
+    value_list = list(value)
+    address = value_list.pop(0)
+    page_no = value_list.pop(0)
+    bbox = value_list[:4]
+    probability = value_list[-1]
     df = df.append({'KEY': key, 'VALUE': address, 'BBOX': bbox, 'PAGE NO': page_no, 'PROBABILITY': probability}, ignore_index=True)
 
 print(df)
