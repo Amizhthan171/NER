0376498e258a662b187a8612a8e59501208a43d1
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-07-10 09:24:56 +0530

    Update Zip.py

--- Zip.py
+++ Zip.py
@@ -1,18 +1,13 @@
-import pandas as pd
+import img2pdf
+import os
 
-# Create a sample dataframe
-df = pd.DataFrame({'Document Type': ['Type A', 'Type B', 'Type A', 'Type B'],
-                   'Value': [10, 20, 30, 40]})
+def convert_tiff_to_pdf(tiff_path):
+    pdf_path = os.path.splitext(tiff_path)[0] + ".pdf"
+    with open(pdf_path, "wb") as output_pdf:
+        output_pdf.write(img2pdf.convert(tiff_path))
 
-# Create an Excel writer
-writer = pd.ExcelWriter('grouped_data.xlsx', engine='xlsxwriter')
+    os.remove(tiff_path)
+    os.rename(pdf_path, tiff_path)
 
-# Group the dataframe by 'Document Type'
-grouped = df.groupby('Document Type')
-
-# Iterate over the groups and save each as a separate sheet
-for name, group in grouped:
-    group.to_excel(writer, sheet_name=name, index=False)
-
-# Save the Excel file
-writer.save()
+# Usage example
+convert_tiff_to_pdf("example.tiff")
