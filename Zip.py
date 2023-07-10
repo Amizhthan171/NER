e901ec68c12e73a6343f5f052bc867594182e252
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-07-10 09:42:57 +0530

    Update Zip.py

--- Zip.py
+++ Zip.py
@@ -1,17 +1,33 @@
-import fitz
-import os
+from PIL import Image
+import pandas as pd
 
-def convert_tiff_to_pdf(tiff_path):
-    pdf_path = os.path.splitext(tiff_path)[0] + ".pdf"
+def crop_and_save_images(tiff_path, dataframe):
+    image = Image.open(tiff_path)
 
-    doc = fitz.open(tiff_path)
-    pdf_bytes = doc.convert_to_pdf()
+    for index, row in dataframe.iterrows():
+        page_no = row['page no']
+        bbox = row['bbox']
+        field = row['field']
 
-    with open(pdf_path, "wb") as output_pdf:
-        output_pdf.write(pdf_bytes)
+        # Convert bbox coordinates to integers
+        bbox = [int(coord) for coord in bbox]
 
-    os.remove(tiff_path)
-    os.rename(pdf_path, tiff_path)
+        # Crop the image using the bbox coordinates
+        image_page = image.copy()
+        image_page.seek(page_no)
+        cropped_image = image_page.crop(bbox)
+
+        # Save the cropped image with the field name as a JPEG
+        save_path = f"{field}.jpg"
+        cropped_image.save(save_path, "JPEG")
 
 # Usage example
-convert_tiff_to_pdf("example.tiff")
+df = pd.DataFrame({
+    'page no': [0, 0, 1, 1],
+    'bbox': [(100, 100, 300, 300), (200, 200, 400, 400), (150, 150, 350, 350), (250, 250, 450, 450)],
+    'field': ['field1', 'field2', 'field3', 'field4']
+})
+
+tiff_file = "example.tiff"
+
+crop_and_save_images(tiff_file, df)
