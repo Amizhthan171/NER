dd9d9aecaba92ef5f023845499e874077d70cd49
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-07-10 13:52:28 +0530

    Update Zip.py

--- Zip.py
+++ Zip.py
@@ -2,10 +2,10 @@ policy_number_value = '123'
 policy_bbox_values = [(1, 2, 3, 4)]
 policy_page_number = 1
 
-bbox_str = ','.join(str(coord) for bbox in policy_bbox_values for coord in bbox)
+bbox_str = [str(coord) for bbox in policy_bbox_values for coord in bbox]
 
 result_dict = {
-    'policy number': [policy_number_value, policy_page_number, bbox_str, 'NA']
+    'policy number': [policy_number_value, policy_page_number, *bbox_str, 'NA']
 }
 
 print(result_dict)
