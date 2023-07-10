a55ea4a3d8951b17c3fe8f3fce2f993570c95eac
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-07-10 09:36:24 +0530

    Update Zip.py

--- Zip.py
+++ Zip.py
@@ -1,10 +1,14 @@
-import img2pdf
+import fitz
 import os
 
 def convert_tiff_to_pdf(tiff_path):
     pdf_path = os.path.splitext(tiff_path)[0] + ".pdf"
+
+    doc = fitz.open(tiff_path)
+    pdf_bytes = doc.convert_to_pdf()
+
     with open(pdf_path, "wb") as output_pdf:
-        output_pdf.write(img2pdf.convert(tiff_path))
+        output_pdf.write(pdf_bytes)
 
     os.remove(tiff_path)
     os.rename(pdf_path, tiff_path)
