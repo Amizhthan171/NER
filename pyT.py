6ac010ed2c53ca492bd8e894fbf38ac329af12f5
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-07-12 17:27:48 +0530

    Update pyT.py

--- pyT.py
+++ pyT.py
@@ -1,46 +1,27 @@
-import PyPDF2
+from exchangelib import Account, Credentials
 
-# Define the region of interest (ROI)
-x0 = 100   # x-coordinate of the upper-left corner of the ROI
-y0 = 100   # y-coordinate of the upper-left corner of the ROI
-x1 = 200   # x-coordinate of the lower-right corner of the ROI
-y1 = 200   # y-coordinate of the lower-right corner of the ROI
+# Set up credentials
+credentials = Credentials(username='your_username', password='your_password')
 
-# Open the PDF file in read binary mode
-with open('file.pdf', 'rb') as pdf_file:
+# Connect to the account
+account = Account(primary_smtp_address='your_email@example.com', credentials=credentials, autodiscover=True)
 
-    # Create a PDF reader object
-    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+# Define the list of sender email addresses to filter
+sender_list = ['sender1@example.com', 'sender2@example.com', 'sender3@example.com']
 
-    # Get the page object for the first page of the PDF file
-    page = pdf_reader.getPage(0)
+# Create an empty list to store recipient addresses
+recipient_list = []
 
-    # Get the dimensions of the page
-    page_width = page.mediaBox.getWidth()
-    page_height = page.mediaBox.getHeight()
+# Traverse through the inbox and subfolders
+for folder in account.inbox.walk():
+    # Filter emails with sender addresses from the sender_list
+    filtered_emails = folder.filter(sender__in=sender_list)
 
-    # Define the ROI in PDF coordinates
-    roi_x0 = x0 / page_width
-    roi_y0 = 1 - (y1 / page_height)
-    roi_x1 = x1 / page_width
-    roi_y1 = 1 - (y0 / page_height)
+    # Traverse through the filtered emails
+    for email in filtered_emails:
+        # Extract recipient addresses and append to the recipient_list
+        for recipient in email.to_recipients:
+            recipient_list.append(recipient.email_address)
 
-    # Define a TextString object for the ROI
-    roi = PyPDF2.pdf.TextString('')
-
-    # Loop through each text object on the page
-    for obj in page['/Contents'].getObject():
-
-        # Check if the object is a TextString
-        if isinstance(obj, PyPDF2.pdf.TextString):
-
-            # Check if the TextString is within the ROI
-            x, y = obj.getXY()
-            if roi_x0 < x < roi_x1 and roi_y0 < y < roi_y1:
-                roi += obj
-
-    # Extract the text from the ROI
-    text = roi.decode('utf-8')
-
-    # Do something with the extracted text
-    print(text)
+# Print the recipient_list
+print(recipient_list)
