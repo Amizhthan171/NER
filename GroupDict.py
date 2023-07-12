8d0c0ace2b6419ad319446cc0f8a0a3550ee1960
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-07-12 15:46:05 +0530

    Update GroupDict.py

--- GroupDict.py
+++ GroupDict.py
@@ -1,46 +1,23 @@
-import pandas as pd
-import zipfile
-import os
-import multiprocessing
+from exchangelib import Account, Credentials, EWSDateTime
+import pytz
+from datetime import timedelta
 
-# Read the CSV file into a pandas DataFrame
-df = pd.read_csv('your_file.csv')
+# Set up credentials
+credentials = Credentials(username='your_username', password='your_password')
 
-# Group the rows by parent ID
-grouped = df.groupby('parent ID')
+# Connect to the account
+account = Account(primary_smtp_address='your_email@example.com', credentials=credentials, autodiscover=True)
 
-# Specify the directory to save the zip files
-save_directory = '/path/to/save/directory/'
+# Calculate the date range for the last 7 days
+end_date = EWSDateTime.now(pytz.utc)
+start_date = end_date - timedelta(days=7)
 
-# Create a new DataFrame to store zipping status
-zipping_status_df = pd.DataFrame(columns=['parent ID', 'Zipping Status'])
-
-# Function to zip files for a group in parallel
-def zip_files(group):
-    parent_id, rows = group
-    zip_file_path = os.path.join(save_directory, f'{parent_id}.zip')
-    try:
-        with zipfile.ZipFile(zip_file_path, 'w') as zipf:
-            for _, row in rows.iterrows():
-                file_path = row['PATHS']
-                filename = os.path.basename(file_path)
-                zipf.write(file_path, arcname=filename)
-        return {'parent ID': parent_id, 'Zipping Status': 'success'}
-    except:
-        return {'parent ID': parent_id, 'Zipping Status': 'failure'}
-
-# Create a multiprocessing pool
-pool = multiprocessing.Pool()
-
-# Map the zip_files function to the grouped DataFrame in parallel
-results = pool.map(zip_files, grouped)
-
-# Close the multiprocessing pool
-pool.close()
-pool.join()
-
-# Convert the results list to a DataFrame
-zipping_status_df = pd.DataFrame(results)
-
-# Write the zipping status DataFrame to a CSV file
-zipping_status_df.to_csv('zipping_status.csv', index=False)
+# Iterate through folders
+for folder in account.inbox.walk():
+    # Fetch emails within the specified date range
+    emails = folder.all().filter(datetime_received__range=(start_date, end_date))
+    
+    # Iterate through filtered emails
+    for item in emails:
+        # Do something with the filtered email, e.g., print subject
+        print(item.subject)
