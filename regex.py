6e585cc0c7f235907c4cece844079398eec6faa7
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-07-25 14:45:12 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,5 +1,4 @@
 import fitz  # PyMuPDF
-import cv2
 
 def has_images(pdf_file):
     pdf_document = fitz.open(pdf_file)
@@ -13,27 +12,10 @@ def has_images(pdf_file):
             xref = img[0]
             image_data = page.get_pixmap(xref=xref)
 
-            # Convert the image data to a NumPy array for OpenCV processing
-            np_array = bytearray(image_data.samples)
-            image_np = np.array(np_array, dtype=np.uint8).reshape(image_data.height, image_data.width, 3)
-
-            # Convert the BGR image to RGB (OpenCV uses BGR by default)
-            rgb_image = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-
-            # Convert the image to grayscale
-            grayscale_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2GRAY)
-
-            # Perform image processing to detect regions that likely contain images
-            # You can adjust the parameters based on your PDFs' characteristics
-            _, threshold_image = cv2.threshold(grayscale_image, 200, 255, cv2.THRESH_BINARY)
-
-            # Find contours in the thresholded image
-            contours, _ = cv2.findContours(threshold_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-            # If any contours are found, consider the page to contain images
-            if len(contours) > 0:
+            # Check if the image has color channels other than grayscale
+            if image_data.n - image_data.alpha > 1:
                 pages_with_images.append(page_num + 1)
-                break  # Break the loop if any image is found on the page
+                break  # Break the loop if any colored image is found on the page
 
     return pages_with_images
 
