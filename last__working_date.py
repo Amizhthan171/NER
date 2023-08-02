c64837006f3a8c4f9c98067a8de248b871b604af
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-08-02 20:40:26 +0530

    Update last__working_date.py

--- last__working_date.py
+++ last__working_date.py
@@ -1,16 +1,15 @@
 import camelot
-import PyPDF4
+import fitz
 
-def read_pdf_with_pypdf4(pdf_path):
-    with open(pdf_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF4.PdfFileReader(pdf_file)
-        num_pages = pdf_reader.numPages
-        text = ""
+def read_pdf_with_pymupdf(pdf_path):
+    pdf_text = ""
+    with fitz.open(pdf_path) as pdf_document:
+        num_pages = pdf_document.page_count
         for page_number in range(num_pages):
-            page = pdf_reader.getPage(page_number)
-            text += page.extract_text()
-    return text
+            page = pdf_document[page_number]
+            pdf_text += page.get_text()
+    return pdf_text
 
 pdf_path = 'path/to/your/pdf_file.pdf'
-pdf_text = read_pdf_with_pypdf4(pdf_path)
+pdf_text = read_pdf_with_pymupdf(pdf_path)
 tables = camelot.read_pdf(pdf_text)
