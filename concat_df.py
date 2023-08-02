92da61782c03ee901a10345d5d698908aa1ceafb
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-08-02 19:52:12 +0530

    Update concat_df.py

--- concat_df.py
+++ concat_df.py
@@ -1,14 +1,40 @@
-import tabula
+import pytesseract
+import cv2
 
-# Replace "path/to/your/table.pdf" with the path to your PDF file
-# Pass the pages parameter to specify the page number(s) with the table(s) you want to extract
-# You can also pass the area parameter to specify the region of the page where the table is located (format: [top, left, bottom, right])
-tables = tabula.read_pdf("path/to/your/table.pdf", pages="all")
+def ocr_table_with_structure(image_path):
+    # Read the image and perform OCR to get the bounding box information
+    image = cv2.imread(image_path)
+    ocr_data = pytesseract.image_to_boxes(image)
 
-# Iterate through the extracted tables and display them
-for idx, table_df in enumerate(tables):
-    print(f"Table {idx + 1}:")
-    print(table_df)
-    print("\n")  # Add a newline between tables
+    # Process the OCR data to extract table content and maintain the structure
+    lines = ocr_data.strip().split('\n')
+    table_data = {}
+    for line in lines:
+        _, x, y, w, h, _ = line.split()
+        row = int(y)
+        if row not in table_data:
+            table_data[row] = []
+        table_data[row].append(line)
 
-# Note:⬤
+    # Sort the table_data by rows
+    sorted_rows = sorted(table_data.keys())
+
+    # Extract text for each row and align the columns
+    table_content = []
+    for row in sorted_rows:
+        row_content = table_data[row]
+        row_content.sort(key=lambda x: int(x.split()[1]))  # Sort by X-coordinate
+        row_text = ' '.join(item.split()[0] for item in row_content)
+        table_content.append(row_text)
+
+    return table_content
+
+# Specify the path to your image containing the table
+image_path = 'path_to_your_image.png'
+
+# Get the table content while preserving the structure
+table_content = ocr_table_with_structure(image_path)
+
+# Print the extracted table content
+for row in table_content:
+    print(row)
