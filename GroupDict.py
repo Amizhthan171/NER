3023ffd0d49ce3780963d65dc6d2f417d7aa7511
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-08-02 17:06:38 +0530

    Update GroupDict.py

--- GroupDict.py
+++ GroupDict.py
@@ -1,23 +1,18 @@
-from exchangelib import Account, Credentials, EWSDateTime
-import pytz
-from datetime import timedelta
+You have a Closing Disclosure document with the following sections on page 2:
+1. Loan Costs
+2. Services Borrower Did Not Shop For
+3. Services Borrower Did Shop For
+4. Total Loan Costs
+5. Taxes and Other Government Fees
+6. Prepaid Expenses
+7. Initial Escrow Payment at Closing
+8. Other Costs
+9. Total Other Costs
+10. Total Closing Costs
 
-# Set up credentials
-credentials = Credentials(username='your_username', password='your_password')
-
-# Connect to the account
-account = Account(primary_smtp_address='your_email@example.com', credentials=credentials, autodiscover=True)
-
-# Calculate the date range for the last 7 days
-end_date = EWSDateTime.now(pytz.utc)
-start_date = end_date - timedelta(days=7)
-
-# Iterate through folders
-for folder in account.inbox.walk():
-    # Fetch emails within the specified date range
-    emails = folder.all().filter(datetime_received__range=(start_date, end_date))
-    
-    # Iterate through filtered emails
-    for item in emails:
-        # Do something with the filtered email, e.g., print subject
-        print(item.subject)
+Please list all the values for each section in the format:
+<Section Name>:
+- Borrower Paid at Closing: <Value>
+- Seller Paid at Closing: <Value>
+- Borrower Paid before Closing: <Value>
+- Seller Paid before Closing: <Value>
