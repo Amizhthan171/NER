8ca29de732f63d5574e12b69bdf50d81ff91a27f
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-08-07 16:37:55 +0530

    Update concat_df.py

--- concat_df.py
+++ concat_df.py
@@ -1,16 +1,23 @@
 from pdf2image import convert_from_path
-import pytesseract
+from PIL import Image
 
-def ocr_pdf(pdf_path):
-    text = ""
+def convert_pdf_to_single_image(pdf_path):
     images = convert_from_path(pdf_path)
 
-    for i, image in enumerate(images):
-        page_text = pytesseract.image_to_string(image, lang='eng')
-        text += page_text + "\n"
+    # Get the dimensions of the first image
+    width, height = images[0].size
 
-    return text
+    # Create a new blank image with the size of the combined pages
+    combined_image = Image.new('RGB', (width, height * len(images)))
+
+    # Merge all the pages into the new image
+    for i, image in enumerate(images):
+        combined_image.paste(image, (0, i * height))
+
+    return combined_image
 
 pdf_path = "/path/to/your/pdf_file.pdf"
-extracted_text = ocr_pdf(pdf_path)
-print(extracted_text)
+output_image = convert_pdf_to_single_image(pdf_path)
+
+# Save the combined image to disk
+output_image.save("/path/to/output_image.jpg", "JPEG")
