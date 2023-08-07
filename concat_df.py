9ff25ed90f7f761c2b65747162c4f0a178532900
Amizhthan171 <75921312+Amizhthan171@users.noreply.github.com>
2023-08-07 16:32:18 +0530

    Update concat_df.py

--- concat_df.py
+++ concat_df.py
@@ -1,40 +1,16 @@
+from pdf2image import convert_from_path
 import pytesseract
-import cv2
 
-def ocr_table_with_structure(image_path):
-    # Read the image and perform OCR to get the bounding box information
-    image = cv2.imread(image_path)
-    ocr_data = pytesseract.image_to_boxes(image)
+def ocr_pdf(pdf_path):
+    text = ""
+    images = convert_from_path(pdf_path)
 
-    # Process the OCR data to extract table content and maintain the structure
-    lines = ocr_data.strip().split('\n')
-    table_data = {}
-    for line in lines:
-        _, x, y, w, h, _ = line.split()
-        row = int(y)
-        if row not in table_data:
-            table_data[row] = []
-        table_data[row].append(line)
+    for i, image in enumerate(images):
+        page_text = pytesseract.image_to_string(image, lang='eng')
+        text += page_text + "\n"
 
-    # Sort the table_data by rows
-    sorted_rows = sorted(table_data.keys())
+    return text
 
-    # Extract text for each row and align the columns
-    table_content = []
-    for row in sorted_rows:
-        row_content = table_data[row]
-        row_content.sort(key=lambda x: int(x.split()[1]))  # Sort by X-coordinate
-        row_text = ' '.join(item.split()[0] for item in row_content)
-        table_content.append(row_text)
-
-    return table_content
-
-# Specify the path to your image containing the table
-image_path = 'path_to_your_image.png'
-
-# Get the table content while preserving the structure
-table_content = ocr_table_with_structure(image_path)
-
-# Print the extracted table content
-for row in table_content:
-    print(row)
+pdf_path = "/path/to/your/pdf_file.pdf"
+extracted_text = ocr_pdf(pdf_path)
+print(extracted_text)
